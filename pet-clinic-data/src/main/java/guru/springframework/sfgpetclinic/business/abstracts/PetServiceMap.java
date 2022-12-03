e0561efe2662fde387a7b8d31ff466e83f61f11a
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-03 13:51:22 +0300

    Impl Map based Services.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/business/abstracts/PetServiceMap.java
@@ -0,0 +1,33 @@
+package guru.springframework.sfgpetclinic.business.abstracts;
+
+import guru.springframework.sfgpetclinic.business.CrudService;
+import guru.springframework.sfgpetclinic.entities.Pet;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/business/abstracts/VetServiceMap.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.business.abstracts;
+
+import guru.springframework.sfgpetclinic.business.CrudService;
+import guru.springframework.sfgpetclinic.business.VetService;
+import guru.springframework.sfgpetclinic.entities.Vet;
+
+import java.util.Set;
+
+public class VetServiceMap extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+}
