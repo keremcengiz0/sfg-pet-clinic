6d0c81b2f9a7b20f37cafffdfaa0e37ee7852566
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-07 12:42:46 +0300

    added specialities, added to vet.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/Speciality.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgpetclinic.entities;
+
+public class Speciality extends BaseEntity {
+    private String description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
