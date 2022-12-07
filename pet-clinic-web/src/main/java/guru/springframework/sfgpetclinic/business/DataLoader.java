73fababcaaaf801b47a6ce8330b75bdba235e68e
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-07 23:10:12 +0300

    added PetTypes to business.

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/business/DataLoader.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/business/DataLoader.java
@@ -3,6 +3,7 @@ package guru.springframework.sfgpetclinic.business;
 import guru.springframework.sfgpetclinic.business.abstracts.OwnerServiceMap;
 import guru.springframework.sfgpetclinic.business.abstracts.VetServiceMap;
 import guru.springframework.sfgpetclinic.entities.Owner;
+import guru.springframework.sfgpetclinic.entities.PetType;
 import guru.springframework.sfgpetclinic.entities.Vet;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
@@ -13,18 +14,28 @@ public class DataLoader implements CommandLineRunner {
 
     private OwnerService ownerService;
     private VetService vetService;
+    private PetTypeService petTypeService;
 
     public DataLoader() {
     }
 
     @Autowired
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+        PetType savedDogType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+        PetType savedCatType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Micheal");
