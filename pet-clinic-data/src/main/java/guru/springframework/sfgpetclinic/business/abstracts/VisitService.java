4b982e781e3e97867818dfae86918c5ef23f9f25
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-10 19:00:50 +0300

    Implemented Map impl for Visit Service.

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/business/abstracts/VisitService.java
@@ -0,0 +1,6 @@
+package guru.springframework.sfgpetclinic.business.abstracts;
+
+import guru.springframework.sfgpetclinic.entities.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
