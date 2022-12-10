ba3ea430cb331ea67c7220cd9a0a6f2803c8ebda
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-10 14:36:40 +0300

    Added Visit Entity.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/Pet.java
@@ -2,6 +2,8 @@ package guru.springframework.sfgpetclinic.entities;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -16,6 +18,9 @@ public class Pet extends BaseEntity {
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
+    private Set<Visit> visits = new HashSet<>();
 
     public String getName() {
         return name;
@@ -47,5 +52,13 @@ public class Pet extends BaseEntity {
 
     public void setOwner(Owner owner) {
         this.owner = owner;
+    }
+
+    public Set<Visit> getVisits() {
+        return visits;
+    }
+
+    public void setVisits(Set<Visit> visits) {
+        this.visits = visits;
     }
 }
