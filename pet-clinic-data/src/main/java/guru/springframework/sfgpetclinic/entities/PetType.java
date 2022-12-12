fdc81b762afb2a4e69a63c524db8445aaf9fdff3
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-12 12:24:54 +0300

    Added Project Lombok to project.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/PetType.java
@@ -1,21 +1,20 @@
 package guru.springframework.sfgpetclinic.entities;
 
+import lombok.*;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
+
     @Column(name = "name")
     private String name;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/entities/Vet.java
@@ -1,21 +1,21 @@
 package guru.springframework.sfgpetclinic.entities;
 
+import lombok.*;
 import javax.persistence.*;
 import java.util.HashSet;
 import java.util.Set;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"), inverseJoinColumns = @JoinColumn(name = "speciality_id"))
     private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getSpecialities() {
-        return specialities;
-    }
-
-    public void setSpecialities(Set<Speciality> specialities) {
-        this.specialities = specialities;
-    }
 }
