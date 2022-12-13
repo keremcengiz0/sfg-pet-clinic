0b65f4ea9a79a12391235a5d51dbd11a1f77da63
keremcengiz0 <keremcengiz0@hotmail.com>
2022-12-13 11:33:51 +0300

    Upgraded to JUnit 5.

---
+++ pet-clinic-web/src/test/java/guru/springframework/sfppetclinic/SfgPetClinicApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.sfppetclinic;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class SfgPetClinicApplicationTests {
+
+    @Test
+    public void contextLoads() {
+
+    }
+}
